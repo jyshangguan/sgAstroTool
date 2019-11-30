f8519ba8913f921b7912d1c95f75a82878da90a6
shangguan <shangguan@mpe.mpg.de>
2019-11-30 22:48:42 +0100

    update utils.py

--- sgAstro/utils.py
+++ sgAstro/utils.py
@@ -1,22 +1,23 @@
+from __future__ import division
 import numpy as np
 __all__ = ["q2InclinationAngle"]
 
 def q2InclinationAngle(q, q0=0.2):
     """
     Calculate the inclination angle from the axis ratio.
-    
+
     Parameters
     ----------
     q : array like
         The axis ratio, b/a.
     q0 : float
         The intrinsic axis ratio when the galaxy is edge-on.
-    
+
     Returns
     -------
     i : array like
         The inclination angle, units: radian.
-    
+
     Notes
     -----
     The equation is from Topal et al. (2018MNRAS.479.3319T), equation (1).
