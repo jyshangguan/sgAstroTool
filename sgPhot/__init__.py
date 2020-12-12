2e1a00854964341bddc5940e2ef9db44fbb749ad
shangguan <shangguan@mpe.mpg.de>
2020-12-12 15:16:00 +0100

    enable ezgal

--- sgPhot/__init__.py
+++ sgPhot/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-#from ezgal_utils import *  # I should replace ezgal with fsps
+from .ezgal_utils import *  # I should replace ezgal with fsps
 #from kcorrection import *  # I should replace ezgal with fsps
 from .stellarmass import *
 from .bandpass import *
